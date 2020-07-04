1b045b88aa334d62d4bc0f0156175512050212ef
cortezd334 <cortezd334@gmail.com>
2020-07-04 10:36:28 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        @artist = self
+        @artist.songs.collect {|song| song.id}.count 
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    @song = self.artist
+    @song.name  
+  end
 end
